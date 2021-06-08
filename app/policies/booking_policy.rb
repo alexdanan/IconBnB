e777987ee78aa5996bc748ec936405ac2923315b
carlos lacueva <lacuevafortitcarlos@gmail.com>
2021-06-08 17:36:25 +0200

    Created the new booking action and view, and also the booking policy and changed the controller accordingly

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,27 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+
+    def index?
+      true
+    end
+
+    def show?
+      true
+    end
+
+    def create?
+      true
+    end
+
+    def new?
+      create?
+    end
+
+    def destroy?
+      record.user == current_user
+    end
+  end
+end
