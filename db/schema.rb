340c90585b9929f90d0759ece207d2272cf83870
Alexia Danan <dananalexia@gmail.com>
2021-06-11 12:33:14 +0200

    fixedmerge

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_06_10_091026) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_091026) do
     t.integer "status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "start_day"
     t.index ["icon_id"], name: "index_bookings_on_icon_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
